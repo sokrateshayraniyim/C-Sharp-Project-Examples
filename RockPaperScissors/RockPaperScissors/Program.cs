f9693126c8289e8b50fbc97bc7579a95b1b1de32
Umut <umtyrdql@gmail.com>
2023-02-26 13:28:16 +0300

    commit to main

---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Taş-kağıt-makas oyunu
+
+            Console.WriteLine("\r\n██████████████████████████████████████████████████████████████████████████████████████████████████████████\r\n█─▄▄▄▄█─▄▄─█▄─█─▄█▄─▄▄▀██▀▄─██─▄─▄─█▄─▄▄─█─▄▄▄▄█─█─██▀▄─██▄─█─▄█▄─▄▄▀██▀▄─██▄─▀█▄─▄█▄─▄█▄─█─▄█▄─▄█▄─▀█▀─▄█\r\n█▄▄▄▄─█─██─██─▄▀███─▄─▄██─▀─████─████─▄█▀█▄▄▄▄─█─▄─██─▀─███▄─▄███─▄─▄██─▀─███─█▄▀─███─███▄─▄███─███─█▄█─██\r\n▀▄▄▄▄▄▀▄▄▄▄▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀▄▀▄▀▄▄▀▄▄▀▀▄▄▄▀▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▄▀▀▄▄▀▄▄▄▀▀▄▄▄▀▀▄▄▄▀▄▄▄▀▄▄▄▀");
+
+            while (true)
+            {
+
+
+
+
+
+                Console.WriteLine("Welcome to Rock-Paper-Scissors!");
+                Console.WriteLine("Please enter your choice: ");
+                Console.WriteLine("1 - Rock");
+                Console.WriteLine("2 - Paper");
+                Console.WriteLine("3 - Scissors");
+
+                // Kullanıcının seçimini alın
+                int userChoice = Convert.ToInt32(Console.ReadLine());
+
+                // Bilgisayarın rastgele bir seçim yapmasını sağlayın
+                Random random = new Random();
+                int computerChoice = random.Next(1, 4);
+
+                // Seçimlerin sonuçlarını yazdırın
+                Console.WriteLine("You chose: " + GetUserChoice(userChoice));
+                Console.WriteLine("The computer chose: " + GetComputerChoice(computerChoice));
+                Console.WriteLine("Result: " + DetermineWinner(userChoice, computerChoice));
+
+                Console.ReadLine();
+            }
+        }
+
+
+
+
+        // Kullanıcının seçimini bir dize olarak döndürür
+        static string GetUserChoice(int userChoice)
+        {
+            if (userChoice == 1)
+            {
+                return "Rock";
+            }
+            else if (userChoice == 2)
+            {
+                return "Paper";
+            }
+            else if (userChoice == 3)
+            {
+                return "Scissors";
+            }
+            else
+            {
+                return "Invalid choice";
+            }
+        }
+
+        // Bilgisayarın seçimini bir dize olarak döndürür
+        static string GetComputerChoice(int computerChoice)
+        {
+            if (computerChoice == 1)
+            {
+                return "Rock";
+            }
+            else if (computerChoice == 2)
+            {
+                return "Paper";
+            }
+            else if (computerChoice == 3)
+            {
+                return "Scissors";
+            }
+            else
+            {
+                return "Invalid choice";
+            }
+        }
+
+        // Kazananı belirler ve sonucu bir dize olarak döndürür
+        static string DetermineWinner(int userChoice, int computerChoice)
+        {
+            if (userChoice == computerChoice)
+            {
+                return "Tie!";
+            }
+            else if ((userChoice == 1 && computerChoice == 3) || (userChoice == 2 && computerChoice == 1) || (userChoice == 3 && computerChoice == 2))
+            {
+                return "You win!";
+            }
+            else
+            {
+                return "The computer wins!";
+            }
+        }
+    }
+}
---
+++ RockPaperScissors/readme.md
@@ -0,0 +1,31 @@
+
+# Rock-Paper-Scissors Game
+
+This is a simple console application written in C# that allows users to play the game of Rock-Paper-Scissors against the computer. The game is played in English, but can easily be adapted to other languages.
+
+## Installation
+
+To run the application, you will need to have the .NET Core SDK installed on your machine. You can download it from the [official .NET website](https://dotnet.microsoft.com/download).
+
+## Usage
+
+To start the game, navigate to the root directory of the application in a terminal or command prompt and type the following command:
+
+```
+dotnet run
+```
+
+
+This will start the game and display a menu of choices for the user. The user should input their choice using the corresponding number (1 for Rock, 2 for Paper, and 3 for Scissors) and press Enter. The computer will then randomly choose a move, and the result of the game will be displayed.
+
+## Contributing
+
+If you would like to contribute to this project, please fork the repository and submit a pull request with your changes. We welcome any contributions that will improve the code, add new features, or fix bugs.
+
+## Contact Me
+
+Instagram : umutyurdugull
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
